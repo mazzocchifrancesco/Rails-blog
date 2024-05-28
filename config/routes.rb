960d82818c169445e2453abf14774db24555f5d5
Francesco Mazzocchi <cesco.mazz@gmail.com>
2024-05-28 09:34:17 +0200

    show first data in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  # definisco vista articles come root path
+  root "articles#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # aggiunta rotta per view articles
+  get "/articles", to: "articles#index"
 end
